8cc692f63e4c057e7464741ed81c08f2e1dc1bbb
Kaynã Gilleady <kg-04@hotmail.com>
2024-01-17 23:07:29 -0300

    variavel de ambiente

--- src/services/BuscaClima.js
+++ src/services/BuscaClima.js
@@ -1,7 +1,9 @@
 /* Busca dados de clima da API e reponde o objeto da resposta */
 
+const KEY = JSON.stringify(import.meta.env.API_KEY)
+
 const buscaClima = async (nameCity) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${nameCity}&appid=${process.env.API_KEY}&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${nameCity}&appid=${KEY}&units=metric`
     let pesquisa = await fetch(url)
     let res = await pesquisa.json()
     return res
