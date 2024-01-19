4df91c363504cb3a2dc983c2602d200f02a10a7b
Kaynã Gilleady <kg-04@hotmail.com>
2024-01-19 16:47:02 -0300

    ajustes

--- src/services/BuscaClima.js
+++ src/services/BuscaClima.js
@@ -1,7 +1,5 @@
 /* Busca dados de clima da API e reponde o objeto da resposta */
 
-const KEY = JSON.stringify(import.meta.env)
-console.log(import.meta.env.mode)
 
 const buscaClima = async (nameCity) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${nameCity}&appid=6cd686b2878d74bcf65fdaff2fd9543a&units=metric`
