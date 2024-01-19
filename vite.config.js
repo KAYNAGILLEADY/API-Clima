0d8950fc068f19b9763bc634b7de6f9c621e9dfe
Kaynã Gilleady <kg-04@hotmail.com>
2024-01-19 16:45:15 -0300

    ajustes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    __APP_ENV__: JSON.stringify(process.env.VITE_PORT)
-  }
 })
